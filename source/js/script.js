cd47d2a2e9e5768d6dee8c58a003e2f6864e523a
Alexandr Pavlychenko <apavlychenko23@gmail.com>
2023-03-27 12:00:23 +0300

    adds header's layout

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,11 @@
+const mainNav = document.querySelector('.page-header__main-nav');
+mainNav.classList.remove('page-header__main-nav--opened');
+const pageHeaderToggle = document.querySelector('.page-header__toggle');
 
+pageHeaderToggle.addEventListener('click', function() {
+  if (mainNav.classList.contains('page-header__main-nav--opened')) {
+    mainNav.classList.remove('page-header__main-nav--opened');
+  } else {
+    mainNav.classList.add('page-header__main-nav--opened');
+  }
+});
