0274efc0ebb7b1fef865045389a2e56cc99ae05f
Alexandr Pavlychenko <apavlychenko23@gmail.com>
2023-04-19 03:50:26 +0300

    corrects errors indicated during the first check

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,9 @@
 const mainNav = document.querySelector('.page-header__main-nav');
-mainNav.classList.remove('page-header__main-nav--opened');
+mainNav.classList.remove('page-header__main-nav--nojs');
 const pageHeaderToggle = document.querySelector('.page-header__toggle');
 
 pageHeaderToggle.addEventListener('click', function() {
-  if (mainNav.classList.contains('page-header__main-nav--opened')) {
-    mainNav.classList.remove('page-header__main-nav--opened');
-  } else {
-    mainNav.classList.add('page-header__main-nav--opened');
-  }
+  mainNav.classList.toggle('page-header__main-nav--opened');
 });
 
 const swiperEl = document.querySelector('.swiper');
